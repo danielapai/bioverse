94c65bec35143cc691553b835e2aec8709b38a2e
abixel <abixel@email.arizona.edu>
2021-06-29 17:42:26 -0600

    switch to sphinx-autoapi

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ release = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.napoleon', 'nbsphinx']
+extensions = ['sphinx.ext.napoleon', 'nbsphinx', 'autoapi.extension']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -69,4 +69,6 @@ nbsphinx_prolog = r"""
 
 autodoc_member_order = 'bysource'
 
-
+autoapi_dirs = ['../']
+autoapi_add_toctree_entry = False
+autoapi_generate_api_docs = False
