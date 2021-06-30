a824fc331c1b0417f6638d27059f966b75a0ac13
abixel <abixel@email.arizona.edu>
2021-06-29 19:08:48 -0600

    added setup files

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+setup(
+    name = "bioverse",
+    version = "1.0",
+    author = "Alex Bixel",
+    author_email = "d.alex.bixel@gmail.com",
+    description = ("A simulation framework to assess the statistical power of future biosignature surveys"),
+    url = "https://github.com/abixel/Bioverse",
+    packages=['bioverse'],
+    include_package_data=True
+)
+
