46250208937ffbc870d386315bb222a0f91870b3
Martin Schlecker <martin.schlecker@mailbox.org>
2022-06-25 15:59:43 +0200

    add a custom function for global magma oceans

--- bioverse/custom.py
+++ bioverse/custom.py
@@ -1,5 +1,51 @@
-""" Define new functions for planet simulation here. """
+""" Define new functions for planet simulation here. Funnction arguments should be provided a default value."""
 
 # Add import statements as necessary
 import numpy as np
 
+
+def M_R(observed):
+    """Just for this test, add a silly mass measurement"""
+    observed['M'] = observed['R']*2
+    return observed
+
+
+def label_lateM(table, Mst_threshold=0.4):
+    # is_lateM=False for all planets
+    table['is_late'] = np.zeros(len(table))
+
+    table['is_late'] = table['M_st'] < Mst_threshold
+    return table
+
+
+def occurrence_hypo(theta, X):
+    """Define the hypothesis that the planet occurrence is lower around late Ms than around early Ms.
+    """
+    return None
+
+
+def magma_ocean(d, f_magma=0.1, a_max=0.1):
+    """Assign a fraction of planets global magma oceans that change the planet's radius.
+
+    Parameters:
+    -----------
+    d: Table
+        The population of planets.
+    f_magma: float
+        The fraction of planets that have global magma oceans.
+    a_max: float
+        The maximum semimajor-axis of the planet's orbit to have a magma ocean.
+
+    Returns
+    -------
+    d : Table
+        Table containing the sample of simulated planets.
+
+    """
+    d['has_magmaocean'] = np.random.binomial(1, f_magma, len(d)).astype(bool)
+    # d.loc[d['a'] > a_max, 'has_magmaocean'] = False
+
+    # reduce the radius of the planets with magma oceans
+    d[d['has_magmaocean']]['R'] *= .8         # HAS TO BE REPLACED WITH MODEL OUTPUT FOR MAGMA OCEAN PLANETS
+
+    return d
