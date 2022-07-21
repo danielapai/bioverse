d3516a763525950f418d9dd32ee1e7af85cc2cc0
Martin Schlecker <martin.schlecker@mailbox.org>
2022-07-21 13:41:00 -0700

    change functional form to exponential cutoff

--- bioverse/custom.py
+++ bioverse/custom.py
@@ -24,7 +24,7 @@ def occurrence_hypo(theta, X):
     return None
 
 
-def magma_ocean(d, f_magma=0.1, a_max=0.1):
+def magma_ocean(d, f_magma=0.1, lambda_a=2., a_cut=0.1):
     """Assign a fraction of planets global magma oceans that change the planet's radius.
 
     Parameters:
@@ -33,8 +33,10 @@ def magma_ocean(d, f_magma=0.1, a_max=0.1):
         The population of planets.
     f_magma: float
         The fraction of planets that have global magma oceans.
-    a_max: float
-        The maximum semimajor-axis of the planet's orbit to have a magma ocean.
+    lambda_a: float
+        Decay parameter for the semi-major axis dependence of having a global magma ocean.
+    a_cut: float
+        cutoff sma for magma oceans. Defines position of the exponential decay.
 
     Returns
     -------
@@ -42,13 +44,9 @@ def magma_ocean(d, f_magma=0.1, a_max=0.1):
         Table containing the sample of simulated planets.
 
     """
-
-    # randomly assign planets to have a magma ocean
-    d['has_magmaocean'] = np.random.binomial(1, f_magma, len(d)).astype(bool)
-
-    # but only for planets with a semimajor-axis less than a_max
-    mask = d['a'] > a_max
-    d['has_magmaocean'][mask] = False
+    # randomly assign planets to have a magma ocean, depending on the semi-major axes
+    P_magma = f_magma * np.exp(-(d['a']/a_cut)**lambda_a) # HAS TO BE REPLACED WITH MODEL OUTPUT FOR MAGMA OCEAN PLANETS
+    d['has_magmaocean'] = np.random.uniform(0, 1, len(d)) < P_magma
 
     # reduce the radius of the planets with magma oceans
     mask = d['has_magmaocean']
