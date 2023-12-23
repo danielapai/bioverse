b076d0beba52578518ede986f37b609494448d71
not a cat <pandinobud@gmail.com>
2023-12-23 19:25:17 +0800

    [fix] add bioverse requirements to setup.py
    This allows pip to automatically install the Bioverse requirements when the user installs Bioverse itself

--- setup.py
+++ setup.py
@@ -3,6 +3,17 @@
 import os
 from setuptools import setup
 
+
+def requirements_list() -> list[str]:
+    reqs_list = []
+
+    with open("requirements.txt", mode="r") as reqs_file:
+        for line in reqs_file:
+            reqs_list.append(line)
+
+    return reqs_list
+
+
 setup(
     name = "bioverse",
     version = "1.1.1",
@@ -11,5 +22,6 @@ setup(
     description = ("A simulation framework to assess the statistical power of future biosignature surveys"),
     url = "https://github.com/danielapai/bioverse",
     packages=['bioverse'],
+    install_requires=requirements_list(),
     include_package_data=True
 )
