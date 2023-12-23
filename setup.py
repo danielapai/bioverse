62492ecd3e0d012274a2671192133093b6808d87
not a cat <pandinobud@gmail.com>
2023-12-23 22:29:34 +0800

    [fix] update the version # for the requires change

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def requirements_list() -> list[str]:
 
 setup(
     name = "bioverse",
-    version = "1.1.1",
+    version = "1.1.2",
     author = "Alex Bixel",
     author_email = "d.alex.bixel@gmail.com",
     description = ("A simulation framework to assess the statistical power of future biosignature surveys"),
