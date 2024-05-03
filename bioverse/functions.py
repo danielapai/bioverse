8e57dcb16000590daaf93226b50561898a013f0a
Noah Tuchow <nwtuchow@gmail.com>
2024-05-03 16:28:42 -0400

    Added read_HPIC to functions

--- bioverse/functions.py
+++ bioverse/functions.py
@@ -328,6 +328,70 @@ def read_stellar_catalog(d, filename='LUVOIR_targets.dat', d_max=30., T_min=0., 
 
     return d
 
+def read_HPIC(d,filename='full_HPIC.txt', m_V_max=None, dist_max=None,
+              required_props=['sy_dist','st_lum','sy_vmag']):
+    """ Generates stars from the HPIC the HWO Preliminary input catalog of Tuchow+, 2024
+    
+    Parameters
+    ----------
+    d : Table
+        Empty table object
+    filename : str, optional
+        Name of the file containing the HPIC
+    m_V_max : float, optional
+        Max V magnitude for simulated stars
+    dist_max : float, optional
+        Max distance for simulated stars. Note the HPIC was constructed with a max dist of 50 pc
+    required_props : list of str, optional
+        Required stellar properties for generated stars. Stars without these properties will be omitted from the target list.
+        Use names from the HPIC not usual bioverse names of properties.
+        The default is ['sy_dist','st_lum','sy_vmag'].
+
+    Returns
+    -------
+    d : Table
+        Table object with generated stars
+
+    """
+    selected_cols= ['star_name','sy_dist','st_mass','st_rad','st_lum','st_teff',
+                'st_spectype','st_age','known_binary_fl','sy_gaiamag','sy_tmag','sy_jmag','sy_vmag']
+    corr_names= ['star_name','d','M_st','R_st','logL','T_eff_st','SpT','age','binary','Gmag','Tmag','Jmag','Vmag']
+    
+    name_dict= dict(zip(selected_cols,corr_names))
+    
+    hpic_dir= filename if os.path.exists(filename) else DATA_DIR + '/' + filename
+    
+    
+    HPIC_df= pd.read_csv(hpic_dir,sep='|',na_values='null',usecols=selected_cols)
+    HPIC_df=HPIC_df.rename(columns=name_dict)
+    
+    if m_V_max!=None:
+        HPIC_df= HPIC_df.loc[HPIC_df['Vmag']<m_V_max]
+        
+    if dist_max!=None:
+        HPIC_df=HPIC_df.loc[HPIC_df['d']<=dist_max]
+    
+    
+    if len(required_props)>0:
+        for i in range(len(required_props)):
+            if i==0:
+                cond= pd.notnull(HPIC_df[required_props[i]])
+                continue
+            cond= np.logical_and(cond,pd.notnull(HPIC_df[required_props[i]]))
+        HPIC_df=HPIC_df.loc[cond]
+    
+    # set columns in data table to be the same as HPIC entries
+    for name in HPIC_df.columns:
+        d[name]= HPIC_df[name].values
+    
+    #add additional columns derived from HPIC
+    d['L_st'] = (10**HPIC_df.logL).values
+    
+    d['starID'] = np.arange(len(d), dtype=int)
+    d['simulated']= np.zeros(len(d), dtype=bool)
+        
+    return d
+
 
 def create_planets_bergsten(d, R_min=1.0, R_max=3.5, P_min=2, P_max=100., transit_mode=False, f_eta=1., seed=42):
     """ Generates planets with periods and radii according to Bergsten+2022 occurrence rate estimates.
