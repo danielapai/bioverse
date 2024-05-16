50aded1b4342317f42431d29c6a605ea54dd686c
not a cat <32406395+the-non-feline@users.noreply.github.com>
2024-05-16 14:49:37 -0700

    increase version to 1.1.6

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def requirements_list() -> list[str]:
 
 setup(
     name = "bioverse",
-    version = "1.1.4",
+    version = "1.1.6",
     author = "Alex Bixel",
     author_email = "d.alex.bixel@gmail.com",
     description = ("A simulation framework to assess the statistical power of future biosignature surveys"),
