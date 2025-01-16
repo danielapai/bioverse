0b176945653c93541ce812a5a0c6a181fe08232d
vivianaugust <vivianaugust@arizona.edu>
2025-01-16 09:43:31 -0700

    Explicitly added searchbox to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -61,6 +61,13 @@ html_theme_options = {
     'collapsiblesidebar': True,
 }
 
+html_sidebars = {
+    '**': [
+        'globaltoc.html',   # Global TOC (site-wide contents)
+        'searchbox.html',   # Search box
+    ]
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
