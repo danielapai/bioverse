15452b2ac7e6c872929990ba0cc20f3b5521a25f
Martin Schlecker <martin.schlecker@mailbox.org>
2025-01-16 20:11:30 -0700

    increase version number

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def requirements_list():
 
 setup(
     name = "bioverse",
-    version = "1.1.6",
+    version="1.2.0",
     author = "Alex Bixel",
     author_email = "d.alex.bixel@gmail.com",
     description = ("A simulation framework to assess the statistical power of future biosignature surveys"),
